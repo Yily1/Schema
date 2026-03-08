eed996646ee7b60869a54a500f0374c264a6fddd
rynierachas08 <rynierachas01@gmial.com>
2026-03-08 20:40:37 +0800

    modified user_logs

--- working_schema.sql
+++ working_schema.sql
@@ -3,14 +3,14 @@ START TRANSACTION;
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
-    date_date DATE DEFAULT CURRENT_DATE
+    date_added DATE DEFAULT CURRENT_DATE
 ) ;
 
 
 CREATE TABLE chickens (
     chicken_id INT AUTO_INCREMENT PRIMARY KEY,
     age INT,
-    date_date  DATE DEFAULT CURRENT_DATE
+    date_added DATE DEFAULT CURRENT_DATE
 );
 
 
